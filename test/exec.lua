7e40b51d383ffdab15a7c3a550296c2218614395
Dhuan Oliveira <dhuanco@gmail.com>
2026-04-21 01:38:22 +0200

    Add test for exec

--- test/exec.lua
+++ test/exec.lua
@@ -1,5 +1,6 @@
 --[[ EXPECTED OUTPUT
 ✅ exec with awk to uppercase text
+✅ exec with failing status code
 END EXPECTED OUTPUT --]]
 
 test("exec with awk to uppercase text", function()
@@ -7,4 +8,10 @@ test("exec with awk to uppercase text", function()
 
     expect_equal(result.output, "FOOBAR")
     expect_equal(result.status, 0)
+end)
+
+test("exec with failing status code", function()
+    local result = exec([[ls this_should_faill]])
+
+    expect_equal(result.status > 0, true)
 end)
