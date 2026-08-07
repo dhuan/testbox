993ae520fb748251634bb8914ee90d329254df4c
Dhuan Oliveira <dhuanco@gmail.com>
2026-08-08 01:19:23 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -93,7 +93,9 @@ fn main() {
         verbose_print_enabled,
     }));
 
-    lua.globals().set("EMPTY_ARRAY", "_TESTBOX_EMPTY_ARRAY_").unwrap();
+    lua.globals()
+        .set("EMPTY_ARRAY", "_TESTBOX_EMPTY_ARRAY_")
+        .unwrap();
 
     add_func(&lua, "expect_equal", expect_equal(ctx.clone()));
     add_func(&lua, "expect_match", expect_match(ctx.clone()));
